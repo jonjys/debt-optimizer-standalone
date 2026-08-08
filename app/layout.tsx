f6c26b35a270b4797bd7ded4c621c9c80fce405e
jonjys <fkornelind@hotmail.com>
2026-08-08 14:11:32 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="sv">
+      <body>{children}</body>
+    </html>
+  );
+}
