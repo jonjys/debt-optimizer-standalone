afc06582b13f4d85f4362f591abf95ff61c38714
jonjys <fkornelind@hotmail.com>
2026-08-09 13:07:32 +0200

    Create types.ts

---
+++ lib/debt-optimizer/types.ts
@@ -0,0 +1,34 @@
+export type AmortizationType = "annuity" | "straight" | "free";
+
+export interface Loan {
+  id: string;
+  name: string;
+  balance: number;
+  interestRate: number;
+  amortizationType: AmortizationType;
+  fixedAmortization?: number;
+  monthlyFee?: number;
+  targetMonthlyPayment?: number;
+}
+
+export interface DebtOptimizerInput {
+  loans: Loan[];
+  monthlyExtraBudget: number;
+  strategy: "avalanche" | "snowball";
+}
+
+export interface PayoffMilestone {
+  loanId: string;
+  loanName: string;
+  payoffDate: string;
+  totalInterestPaid: number;
+}
+
+export interface DebtOptimizerResult {
+  totalOriginalInterest: number;
+  totalOptimizedInterest: number;
+  totalSavings: number;
+  monthsSaved: number;
+  freedomDate: string;
+  milestones: PayoffMilestone[];
+}
